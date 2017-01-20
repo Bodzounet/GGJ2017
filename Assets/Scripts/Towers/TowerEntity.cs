76c8a7e3d01de76df5aabea9b4109d77c19f0353
Bodz <jean.boulot@epitech.eu>
2017-01-21 00:05:47 +0100

    tower entity, xraytower

---
+++ Assets/Scripts/Towers/TowerEntity.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class TowerEntity : MonoBehaviour
+{
+    [SerializeField]
+    private float _baseDommages;
+    public float BaseDommages
+    {
+        get { return _baseDommages; }
+    }
+
+    private float _dommages;
+    public float Dommages
+    {
+        get { return _dommages; }
+        set
+        {
+            _dommages = value;
+        }
+    }
+
+    [SerializeField]
+    private float _baseAttackSpeed;
+    public float BaseAttackSpeed
+    {
+        get { return _baseAttackSpeed; }
+    }
+
+    private float _attackSpeed;
+    public float AttackSpeed
+    {
+        get { return _attackSpeed; }
+        set
+        {
+            _attackSpeed = value;
+        }
+    }
+
+    [SerializeField]
+    private int _baseGoldCost;
+    public int BaseGoldCost
+    {
+        get { return _baseGoldCost; }
+    }
+
+    private int _goldCost;
+    public int GoldCost
+    {
+        get { return _goldCost; }
+        set
+        {
+            _goldCost = value;
+        }
+    }
+
+    [Range(0, 100)]
+    [SerializeField]
+    protected int _bonusPercentagePerNextLevel;
+
+    private int _level = 0;
+    public virtual int Level
+    {
+        get { return _level; }
+        protected set
+        {
+            _level = value;
+            AttackSpeed = BaseAttackSpeed * (1 + _bonusPercentagePerNextLevel * (_level - 1) / 100.0f); 
+            Dommages = BaseDommages * (1 + _bonusPercentagePerNextLevel * (_level - 1) / 100.0f);
+            GoldCost = Mathf.FloorToInt(BaseGoldCost * (1 + _bonusPercentagePerNextLevel * (_level - 1) / 100.0f));
+        }
+    }
+
+    public void UpdateLevel()
+    {
+        Level++;
+    }
+
+    protected virtual void Start()
+    {
+        Level = 1;
+    }
+
+    protected abstract void Ivk_Attack();
+}
---
+++ Assets/Scripts/Towers/XRayTower.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class XRayTower : TowerEntity
+{
+    private bool _isFighting = false;
+    private List<GameObject> _targets = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Mob"))
+        {
+            _targets.Add(col.gameObject);
+            if (!_isFighting)
+            {
+                Debug.Log(1 / AttackSpeed);
+                InvokeRepeating("Ivk_Attack", 0, 1 / AttackSpeed);
+                _isFighting = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Mob"))
+        {
+            _targets.Remove(col.gameObject);
+            if (_targets.Count == 0)
+            {
+                CancelInvoke("Ivk_Attack");
+                _isFighting = false;
+            }
+        }
+    }
+
+    protected override void Ivk_Attack()
+    {
+        Debug.Log("j'attaque");
+    }
+}
