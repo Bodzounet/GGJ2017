b7d44f7099f30e5a983d1abbe69fa2dbd545bf9a
Bodz <jean.boulot@epitech.eu>
2017-01-20 21:00:48 +0100

    controller & package

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        JoyStickController jc = GetComponent<JoyStickController>();
+
+        j
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
