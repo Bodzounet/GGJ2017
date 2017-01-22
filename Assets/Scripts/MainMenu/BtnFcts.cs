d3e014f2b8614dd16322cb5520d54fea1564451d
Bodz <jean.boulot@epitech.eu>
2017-01-22 09:09:49 +0100

    main menu

---
+++ Assets/Scripts/MainMenu/BtnFcts.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BtnFcts : MonoBehaviour
+{
+    public GameObject credits;
+    public SelectMenuButton selectMenuButton;
+
+    void Start()
+    {
+        selectMenuButton.OnSelectedButtonChanged += SelectMenuButton_OnSelectedButtonChanged;
+    }
+
+    private void SelectMenuButton_OnSelectedButtonChanged(int id)
+    {
+        if (id != 2)
+            credits.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+    }
+}
